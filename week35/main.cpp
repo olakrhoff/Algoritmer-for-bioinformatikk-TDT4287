0ebb7458bbe945753a507e1e9e7ba0e89e2bfa95
olakrhoff <ola.kr@hotmail.no>
2022-08-31 14:43:44 +0200

    Feat: Cloud sync

--- week35/main.cpp
+++ week35/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdint.h>
+#include <cstdint>
 
 std::string lcs(std::string a_S, std::string a_R);
 
@@ -7,14 +7,22 @@ int main(int argc, char** argv)
 {
     std::cout << lcs("ATTCGGTTA", "TAGTGATG") << std::endl;
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
+uint16_t coordToIndex(uint16_t a_row, uint16_t a_col, uint16_t a_rowLen)
+{
+    return a_row + a_col * a_rowLen;
+}
 
 std::string lcs(std::string a_S, std::string a_R)
 {
     uint8_t* dp_matrix = new uint8_t[a_S.length() * a_R.length()];
     
-
+    dp_matrix[coordToIndex(0, 0, a_R.length())] = 0;
+    
+    
+    
+    delete[] dp_matrix;
     return "";
 }
